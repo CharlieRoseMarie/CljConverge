a731241a2c5f9c7e232fad05012cd8b4a0620fae
TAllen <takev.takev@gmail.com>
2015-04-14 20:39:42 -0700

    Created the CljConverge function

--- src/clj_converge/core.clj
+++ src/clj_converge/core.clj
@@ -1,6 +1,18 @@
-(ns clj-converge.core)
+(ns clj-converge.core
+  (:require [clojure.math.numeric-tower :as math]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(set! *warn-on-reflection* true)
+
+(defn- check-threshold [values threshold]
+  (let [[old new] values]
+    (<= (math/abs (- old new)) threshold)))
+
+(defn CljConverge
+  ([modfn arg threshold]
+   (let [initial (modfn arg)]
+    (first (map second (drop-while #(not (check-threshold % threshold))
+                       (iterate (fn [[p n]] [n (modfn n)]) [initial (modfn initial)]))))))
+  ([modfn threshfn arg threshold]
+   (let [initial (modfn arg)]
+    (first (map second (drop-while #(not (threshfn % threshold))
+                       (iterate (fn [[p n]] [n (modfn n)]) [initial (modfn initial)])))))))
