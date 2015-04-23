474caffe37f40496fd52b5b2ad2f5e239f819b7d
TAllen <takev.takev@gmail.com>
2015-04-23 02:01:13 -0700

    Added a function to converge sequences and series

--- src/clj_converge/core.clj
+++ src/clj_converge/core.clj
@@ -10,9 +10,17 @@
 (defn CljConverge
   ([modfn arg threshold]
    (let [initial (modfn arg)]
-    (first (map second (drop-while #(not (check-threshold % threshold))
-                       (iterate (fn [[p n]] [n (modfn n)]) [initial (modfn initial)]))))))
+     (first (map second (drop-while #(not (check-threshold % threshold))
+                                    (iterate (fn [[p n]] [n (modfn n)]) [initial (modfn initial)]))))))
   ([modfn threshfn arg threshold]
    (let [initial (modfn arg)]
-    (first (map second (drop-while #(not (threshfn % threshold))
-                       (iterate (fn [[p n]] [n (modfn n)]) [initial (modfn initial)])))))))
+     (first (map second (drop-while #(not (threshfn % threshold))
+                                    (iterate (fn [[p n]] [n (modfn n)]) [initial (modfn initial)])))))))
+
+(defn SeqConverge [seq-function initial inc-function joining-function threshold]
+    (loop [previous-value (seq-function initial)
+           current-nth (inc-function (inc-function initial))
+           current-value (joining-function previous-value (seq-function (inc-function initial)))]
+      (if (check-threshold [previous-value current-value] threshold)
+        current-value
+        (recur current-value (inc-function current-nth) (joining-function current-value (seq-function current-nth))))))
